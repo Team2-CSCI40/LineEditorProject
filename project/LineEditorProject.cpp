f1e7f7d1b8a734bc59ce3cf57e2d43aad2481bd4
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2013-11-09 20:59:30 -0800

    Substitute is operational
    Substitute is working. Hopefully it is working to Jamison's
    specifications.
    Will add 'Save' (because I found my notes on writing to text files) and
    error checking tomorrow.
    Keep up the good work.

--- project/LineEditorProject.cpp
+++ project/LineEditorProject.cpp
@@ -10,11 +10,11 @@
 #include <fstream>
 using namespace std;
 
-const bool DEBUG = true;
+const bool DEBUG = false;
 const int MAX = 100;
 
 int Reader(string);
-void Substitute(); // ASSIGNED TO Levi/Atef - Difficult
+void Substitute(string [], int, string,int); // ASSIGNED TO Levi/Atef - Difficult
 void Type(string[], int, int&); 
 void Copy();
 void Paste();
@@ -61,7 +61,7 @@ int main() // Main program
 						break;
 					case 'S': 
 						inputValue=Reader(inputString);
-						Substitute();
+						Substitute(txtFile,currentLineIndex,inputString,total);
 					
 					// if a case has two possible functions	
 						/*
@@ -189,13 +189,102 @@ int Reader(string inputString) // You will have to add more conditions for subst
 		return inputValue;
 	}
 	
-void Substitute()
+void Substitute(string txtfile[MAX],int current_line,string input_string,int total_line)
 	{
 		if(DEBUG)
 			{
 				cout<<"'Substitute' has been called. "<<endl;
 			}
+			int pos, pos_2, pos_3, pos_old_string, txtfile_length;
+			pos = input_string.find('/');
+			string temp_1, new_string, temp_2, old_string;
+			temp_1 = input_string.substr(pos + 1);
+			pos_2 = temp_1.find('/');
+			old_string = temp_1.substr(0,pos_2);
+			if(DEBUG)
+				cout<<"This is the old string: "<<old_string<<endl;
+			temp_2 = temp_1.substr(pos_2+1);
+			new_string = temp_2.substr(0,temp_2.length()-1);
+			if(DEBUG)
+			{
+				cout<<"This is the new string: "<<new_string<<endl;
+			}
+			int i_for_loop;
+			int new_string_length,old_string_length;
+			old_string_length = old_string.length();
+			new_string_length = new_string.length();
+			bool check;
+			int start_from ;
+			i_for_loop = current_line;
+			//for(i_for_loop = current_line; i_for_loop <= total_line;i_for_loop++)
+			//{
+						start_from = 0;
+						check = true;
+						string the_original_string = txtfile[i_for_loop];
+						if(txtfile[i_for_loop].find(old_string) == -1)//to show if no match string for the old_string.
+						{
+							cout<<"Line number "<< i_for_loop<<":"<<" have no '"<<old_string<<"'"<<endl;
+						}
+						if(txtfile[i_for_loop].empty() == true)//to leave if it empty.
+						{
+							cout<<"Line number "<<i_for_loop<<" is EMPTY."<<endl;
+							check = false;
+						}
+						txtfile_length = txtfile[i_for_loop].length();
+						//pos_old_string = txtfile[i_for_loop].find(old_string,start_from);
+						//cout<<"the pos_old_string= "<<pos_old_string<<endl;
+						/*if(pos_old_string == -1)
+						{
+							check = false;
+						}*/
+						while(check)
+						{
+					 	txtfile_length = txtfile[i_for_loop].length();
+					 	if(DEBUG)
+					 		{cout<<"This is the string length: "<<txtfile_length<<endl;}
+				 		pos_old_string = txtfile[i_for_loop].find(old_string,start_from);//the position of the old_string in the string.
+				 		if(DEBUG)
+				 			{cout<<"this is the position of old string: "<<pos_old_string<<endl;}
+				 		if(pos_old_string == -1)
+				 			{
+					 			break;
+				 			}
+				 		if(new_string.empty() == true)
+				 		{
+					 		txtfile[i_for_loop].replace(pos_old_string,old_string_length,"");
+					 		if(DEBUG)
+					 			cout<<"What happened: "<<txtfile[i_for_loop]<<endl;
+					 		start_from = 0;
+				 		}
+				 		else
+				 		{txtfile[i_for_loop].replace(pos_old_string,old_string_length,new_string);
+				 		if(DEBUG)
+				 			{cout<<"This is the new string: "<<txtfile[i_for_loop]<<endl;}
+					 	//start_from = pos_old_string + old_string_length +1;
+				 		if(txtfile[i_for_loop].at(pos_old_string + new_string_length - 1) == '\0')
+				 		{
+					 		check = false;
+				 		}
+				 		else
+				 		{
+					 		start_from = pos_old_string + new_string_length;
+				 		}
+			 			}
+				 		if(DEBUG)
+				 		{cout<<"The position of the start_from in the txtfile: "<<start_from<<endl;}
+						}
+						if(txtfile[i_for_loop] != the_original_string)
+						cout<<"This is line number "<<i_for_loop<<" after substiute :" <<txtfile[i_for_loop]<<endl;
+		//}
+				
+			if(DEBUG)
+			{
+				cout<<"Position of the first / in the string: "<<pos<<endl;
+				//cout<<"This is show the position of the first letter in the wanted array with the whole string:  "<<pos_2<<"\n"<<temp_1<<endl;
+			}
+			
 	}
+
 
 void Type(string txtFile[], int type_number, int&currentLineIndex) 
 	{ 
